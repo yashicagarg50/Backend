58ed96dacd9a7002d631adf54be4f1a47d2b1357
yashicagarg50 <97343839+yashicagarg50@users.noreply.github.com>
2022-12-07 20:01:40 +0530

    changes

--- Express/app.js
+++ Express/app.js
@@ -35,3 +35,6 @@ app.use((req, res)=>{
     res.status(404).sendFile('./views/404.html',{root:__dirname});
    });
 
+
+
+  
---
+++ HTTP_Methods/methods.js
@@ -0,0 +1,43 @@
+const express=require("express");
+
+const app=express();
+//middleware func-->json
+app.use(express.json());
+app.listen(3000);
+
+//get
+ let users={};
+
+ app.get('/users',(req,res)=>{
+    res.send(users);
+ })
+
+ app.post('/users',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        message:"data received successfully",
+        users:req.body
+    });
+ });
+
+ //update ==> patch
+ app.patch('/users',(req,res)=>{
+    console.log('req.body-> ',req.body);
+    //update data in users obj
+    let dataToBeUpdated=req.body;
+    for(key in dataToBeUpdated)
+    {
+        user[key]=dataToBeUpdated[key];
+    }
+    res.json({
+        message:"data updated successfully"
+    });
+ });
+
+ //to delete a data
+ app.delete('/user',(req,res)=>{
+    users={};
+    res.json({
+        message:"data has been deleted"
+    });
+ });
